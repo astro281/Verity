6dbe9ffe45ddd322fb71d185b2ad21a5ee31530d
twst <you@example.com>
2026-06-27 12:58:11 -0500

    Align client-side imports with Mojang Mappings

--- src/client/java/com/veritymod/client/renderer/VerityEntityRenderer.java
+++ src/client/java/com/veritymod/client/renderer/VerityEntityRenderer.java
@@ -2,17 +2,17 @@ package com.veritymod.client.renderer;
 
 import com.veritymod.VerityMod;
 import com.veritymod.entity.VerityEntity;
-import net.minecraft.client.render.entity.EntityRenderer;
-import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.util.Identifier;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
 
 public class VerityEntityRenderer extends EntityRenderer<VerityEntity> {
-    public VerityEntityRenderer(EntityRendererFactory.Context context) {
+    public VerityEntityRenderer(EntityRendererProvider.Context context) {
         super(context);
     }
 
     @Override
-    public Identifier getTexture(VerityEntity entity) {
+    public ResourceLocation getTextureLocation(VerityEntity entity) {
         return VerityMod.id("textures/entity/verity.png");
     }
 }

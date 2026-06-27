20a146c330acf97044f92226881c27326216f2c1
twst <you@example.com>
2026-06-27 12:27:36 -0500

    Initial commit

---
+++ src/client/java/com/veritymod/client/VerityModClient.java
@@ -0,0 +1,10 @@
+package com.veritymod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VerityModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}

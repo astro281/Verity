96d97793f38a3b76aca5caa48bd7ae8140dcfc38
astro281 <asr3o1235@gmail.com>
2026-06-27 13:04:09 -0500

    Update VerityEntity.java

--- src/main/java/com/veritymod/entity/VerityEntity.java
+++ src/main/java/com/veritymod/entity/VerityEntity.java
@@ -1,11 +1,33 @@
 package com.veritymod.entity;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.PathAwareEntity;
-import net.minecraft.world.level.World;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
-public class VerityEntity extends PathAwareEntity {
-    public VerityEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
-        super(entityType, world);
-    }
+public class VerityEntity extends Animal {
+
+	public VerityEntity(EntityType<? extends Animal> entityType, Level level) {
+		super(entityType, level);
+	}
+
+	@Override
+	protected void registerGoals() {
+		this.goalSelector.addGoal(0, new FloatGoal(this));
+		this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 1.0));
+		this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 6.0f));
+		this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
+	}
+
+	@Override
+	public @Nullable Animal getBreedOffspring(ServerLevel serverLevel, AgeableMob ageableMob) {
+		return null;
+	}
 }
